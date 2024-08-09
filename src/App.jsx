458df83b8a50708d711ac24a7dd05aac1c1401a8
Htet Wai Yan Aung <waiyan@andfun.biz>
2024-08-10 02:38:49 +0700

    Finish React State, Props and Components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import List from './List.jsx'
+import Item from './Item.jsx'
+
+export default function App()
+{
+  const [data, setData] = useState([
+    { id: 1, content: 'Hello, World', name: 'Alice'},
+    { id: 2, content: 'React is Fun! OMG', name: 'Liam'},
+    { id: 3, content: 'Yay, Interesting', name: 'Bob'},
+  ])
+
+  const remove = id => {
+    setData(data.filter(item => item.id !== id))
+  }
+
+  return (
+    <div style={{ maxWidth: 600, margin: '20px auto' }}>
+      <h1>Yaycha</h1>
+      <List>
+        {data.map( item => <Item item={item} remove={remove}></Item>)}
+      </List>
+    </div>
+  )
+}
---
+++ src/Item.jsx
@@ -0,0 +1,9 @@
+export default function Item({ item, remove })
+{
+  return (
+      <li style={{ padding: 10, borderBottom: '1px solid #ddd', display: 'flex', justifyContent: 'space-between'}}>
+        <span>{item.content} - <b>{item.name}</b></span>
+        <button onClick={() => remove(item.id)}>X</button>
+      </li>
+    )
+}
